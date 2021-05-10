2a6471df8c784e30e02a00b3aff655c520b8f71b
Gustavo Jakobi <gujakobi10@gmail.com>
2021-05-10 15:13:29 -0300

    terminando as views

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,10 +9,29 @@
     </div>
 </div>
 
+@if(session()->has('message'))
+    <div class="w-4/5 m-auto mt-10 pl-2">
+        <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4">
+            {{session()->get('message')}}
+        </p>
+    </div>
+@endif
+
+@if(Auth::check())
+    <div class="pt-15 w-4/5 m-auto">
+        <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+            Create Post
+        </a>
+    </div>
+
+@endif
+
+
+
 @foreach ($posts as $post)
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305_960_720.jpg" alt="" width="700">
+            <img src="{{ asset('images/' . $post->image_path) }}" alt="" width="700">
         </div>
         <div>
             <h2 class="text-gray-700 font-bold text-5xl pb-4">
@@ -30,6 +49,26 @@
             <a href="/blog/{{$post->slug}}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
                 Keep Reading
             </a>
+
+            @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                <span class="float-right">
+                    <a href="/blog/{{$post->slug}}/edit" class="text-gray-800 italic hover:text-gray-900 pb-1 border-b-2">
+                        Edit
+                    </a>
+                </span>
+
+                <span class="float-right">
+                    <form
+                        action="/blog/{{$post->slug}}"
+                        method="POST">
+                        @csrf
+                        @method('delete')
+                        <button class="text-red-500 pr-3" type="submit">
+                            Delete
+                        </button>
+                    </form>
+                </span>
+            @endif
         </div>
     </div>
 @endforeach
